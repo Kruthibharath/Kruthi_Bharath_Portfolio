0603f428146c047d8093c2e6ecae0b0167d41c73
Kruthibharath <kruthi.bharath@gmail.com>
2024-01-31 17:15:10 +0000

    refactor:altered home page with styling

--- my-portfolio/src/pages/Home/Home.tsx
+++ my-portfolio/src/pages/Home/Home.tsx
@@ -1,11 +1,29 @@
+import { SocialMedia } from "../../components/SocialMedia/SocialMedia";
+import heroImage from "../../assets/images/kruthi.jpg";
+import "./Home.scss";
+
 export const Home: React.FC = () => {
   return (
     <>
-      <p>
-        Hi, I am Kruthi Bharath looking for the Front End Web Developer position
-        in the London area. This is my portfolio to showcase my skills,
-        experience and projects I have worked so far.
-      </p>
+      <section className="hero">
+        <div className="hero_text-wrapper">
+          <h1>Kruthi Bharath</h1>
+          <h2>Front End Web Developer</h2>
+          <p>
+            Hi, I am Kruthi Bharath looking for the Front End Web Developer
+            position in the London area. This is my portfolio to showcase my
+            skills, experience and projects I have worked so far.
+          </p>
+          <SocialMedia />
+        </div>
+        <div className="hero_image-wrapper">
+          <img
+            className="hero_image"
+            alt="Image of Kruthi Bharath"
+            src={heroImage}
+          />
+        </div>
+      </section>
     </>
   );
 };
